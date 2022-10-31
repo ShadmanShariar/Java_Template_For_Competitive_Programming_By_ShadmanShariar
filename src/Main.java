fb3bd953edcc7b78b6e038bf2ec45dc4ae8469d9
Shadman Shariar <75669228+ShadmanShariar@users.noreply.github.com>
2022-10-31 23:27:16 +0600

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -96,6 +96,12 @@ for (int tt = 1; tt <= tc; tt++) {
 }
 //===========================================================================================//
 //-------->> Temporary Method Starts Here <<--------//
+public static LinkedList<String>ps=new LinkedList<String>();
+public static void permute(String s , String answer)
+{if (s.length() == 0){ps.add(answer);answer="";return;}
+for(int i = 0 ;i < s.length(); i++){char ch = s.charAt(i);
+String left_substr=s.substring(0,i);String right_substr=s.substring(i+1);
+String rest = left_substr + right_substr;permute(rest, answer + ch);}}
 private static int[] nodes;
 public static void setsizeooofunion(int size) {
     nodes = new int[size];

624d1dc381f08e5a00965b69b602aad9784590fe
Shadman Shariar <75669228+ShadmanShariar@users.noreply.github.com>
2023-07-30 01:50:07 +0600

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -102,28 +102,12 @@ for (int tt = 1; tt <= tc; tt++) {
 //-------->> Temporary Method Starts Here <<--------//
 //-------->>  Temporary Method Ends Here  <<--------//
 //===========================================================================================//
-private static int upper_bound(int[] nums, int target) {
-    int low = 0, high = nums.length - 1;
-    while(low < high) {
-        int mid = low + (high+1 - low) / 2;
-        if(nums[mid] > target)
-            high = mid - 1;
-        else
-            low = mid;
-    }
-    return nums[low] == target? low : -1;
-}
-private static int lower_bound(int[] nums, int target) {
-    int low = 0, high = nums.length - 1;
-    while(low < high) {
-        int mid = low + (high - low) / 2;
-        if(nums[mid] < target)
-            low = mid + 1;
-        else
-            high = mid;
-    }
-    return nums[low] == target? low : -1;
-}
+private static int upper_bound(int[]nums,int target){int low=0,high=nums.length-1;
+while(low < high) {int mid = low + (high+1 - low) / 2;if(nums[mid] > target)
+high = mid - 1;else low = mid;}return nums[low] == target? low : -1;}
+private static int lower_bound(int[]nums,int target){int low=0,high=nums.length-1;
+while(low < high) {int mid = low + (high - low) / 2;if(nums[mid] < target)
+low = mid + 1; else high = mid;}return nums[low] == target? low : -1;}
 private static int[] nodes;public static void setsizeofunion(int size)
 {nodes = new int[size];for (int i = 0; i < size; i++) {nodes[i] = i;}}
 public static int root (int i){while(i!=nodes[i])i=nodes[i];return i;}
